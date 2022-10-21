3a41f7b8bdc9e26bd16d9dd9dfc1732d5ba1d455
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 <e.sharangab@alustudent.com>
2022-10-21 09:16:51 -0700

    pyscripts

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+word = "Holberton"
+word_first_3 = [0:3]
+world_last_2 = [-2:9]
+middle_world = [1:9]
+
+print(f"First 3 letters: {word_first_3}")
+
+print(f"Last 2 letters: {word_last_2}")
+
+print(f"Middle word: {middle_word}")
