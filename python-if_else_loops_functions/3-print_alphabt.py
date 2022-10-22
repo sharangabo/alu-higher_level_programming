6f4a45fb293afcdc8df58bd2f5c70dca618fa650
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 <e.sharangab@alustudent.com>
2022-10-22 06:56:05 -0700

    print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    if chr(i) == 'q' or chr(i) == 'e': 
+    if chr(i) == 'q' or chr(i) == 'e':
 
         continue
     print("{}".format(chr(i)), end="")
