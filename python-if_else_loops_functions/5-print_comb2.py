da7fe4e1faa87b9f459f385f4cce3031de3baae7
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 <e.sharangab@alustudent.com>
2022-10-22 05:49:02 -0700

    comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+for i in range(0, 100):
+
+        if i <= 98:
+
+                    print("{:02d}".format(i), end=", ")
+
+                        else:
+
+                                    print(99)
