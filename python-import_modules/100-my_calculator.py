77c7ae82226ec1bcb9935b868b0a19f51bed4758
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 <e.sharangab@alustudent.com>
2022-10-23 04:54:11 -0700

    commit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,3 +8,20 @@ if __name__ == "__main__":
     if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+
+        if op == "+":
+            num = add(a, b)
+        elif op == "-":
+            num = sub(a, b)
+        elif op == "*":
+            num = mul(a, b)
+        elif op == "/":
+            num = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        print("{:d} {} {:d} = {:d}".format(a, op, b, num))
