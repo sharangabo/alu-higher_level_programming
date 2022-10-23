e5e493db53f3bfc3ebc9486d608a28c32df468b7
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 <e.sharangab@alustudent.com>
2022-10-23 05:30:58 -0700

    commit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,15 +4,12 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     args = len(argv)
-
     if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
         a = int(argv[1])
         op = argv[2]
         b = int(argv[3])
-
         if op == "+":
             num = add(a, b)
         elif op == "-":
@@ -24,4 +21,4 @@ if __name__ == "__main__":
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
-        print("{:d} {} {:d} = {:d}".format(a, op, b, num))
+            print("{:d} {} {:d} = {:d}".format(a, op, b, num))
