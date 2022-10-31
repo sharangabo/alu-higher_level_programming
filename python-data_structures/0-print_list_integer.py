2e7041c683031811532a7606869e267abc033b47
sharangabo <e.sharangab@alustudent.com>
2022-10-31 21:48:34 +0200

    print integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     print_list_integer = __import__('0-print_list_integer').print_list_integer
-    my_list = [1, 2, 3]
+    my_list = ['1', '2', '3']
     print_list_integer = (my_list)
 print(print_list_integer)    
