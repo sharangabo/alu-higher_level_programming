0ec16af8587d7b1aef1479e740af218831c80fdc
sharangabo <e.sharangab@alustudent.com>
2022-11-01 11:35:59 +0200

    secure elemrnt in list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list)
+    elif idx >= len(my_list)
         return None
     else:
         return my_lisr[idx]
