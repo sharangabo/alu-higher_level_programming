0adddc374c7d9f539a4076d8cb385bb17d5546ff
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 <e.sharangab@alustudent.com>
2022-11-08 05:50:16 -0800

    raise a message

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+def raise_exception_msg(message=""):
+    raise NameError(message)
