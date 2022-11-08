916e757cafaba1db03c27670cfa42aac2fab204c
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 <e.sharangab@alustudent.com>
2022-11-08 02:45:32 -0800

    safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for i in renge(x):
+            print(my_list[i], end='')
+            printed_count += 1
+    except:
+        pass
+    print()
+    return printed_count
