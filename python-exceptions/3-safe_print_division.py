e124ad71622b7f49a5a152f0a4ec3c029e356d20
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 <e.sharangab@alustudent.com>
2022-11-08 03:51:27 -0800

    Division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,6 +3,8 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = None
+    except:
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
