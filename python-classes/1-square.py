eebfd4759f638d7c32582f960bdb1b5f606f1e24
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 <e.sharangab@alustudent.com>
2022-11-09 01:08:06 -0800

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Create a square
+
+
+class Square:
+    '''
+    Create a square
+    '''
+
+    def __init__(self, size):
+        ''' init size '''
+        self.__size = size
