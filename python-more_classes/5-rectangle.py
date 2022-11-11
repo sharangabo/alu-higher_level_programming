8c575601ba1b1b81c226d787ee0ba80ede96b04e
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 <e.sharangab@alustudent.com>
2022-11-11 07:13:57 -0800

    commit

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -69,3 +69,7 @@ class Rectangle:
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
