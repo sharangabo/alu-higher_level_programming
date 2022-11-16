cd9a4b0c61c6cd0a4cee699fa568cce34d35d093
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 <e.sharangab@alustudent.com>
2022-11-16 13:29:29 -0800

    commit

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-    '''Check if the object is an instance of a class'''
-
-     return isinstance(obj, a_class)
+    '''Check if the object is an instance of a class
+    '''
+    return isinstance(obj, a_class)
