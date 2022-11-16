1c31ba88794c364b8265dcf5af030f46145a1e25
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 <e.sharangab@alustudent.com>
2022-11-16 11:42:43 -0800

    commit

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,17 +9,17 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-        representation of 'Square'
-    """
+"""
+    representation of 'Square'
+"""
 
     def __init__(self, size):
-        """
-            Initialize a new 'Square'
-            Args:
-                size (int): size of the new square
-        """
+    """
+        Initialize a new 'Square'
+        Args:
+            size (int): size of the new square
+    """
 
-        self.integer_validator("size", size)
+        `self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
