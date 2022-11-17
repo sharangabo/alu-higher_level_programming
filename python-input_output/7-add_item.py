b0d8c1e8742e83f9de912839a0ff7416a712d3d8
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 <e.sharangab@alustudent.com>
2022-11-17 10:34:35 -0800

    commmit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list, and then save them to a file"""
-
-
+"""add_item
+"""
 import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+    loadFile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    loadFile = []
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file.py') \
-        .load_from_json_file
-
-    try:
-        loadfile = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        loadfile = []
-
-    arg = len(sys.argv)
-    for i in range(1, arg):
-        loadfile.append(sys.argv[i])
-    save_to_json_file(loadfile, "add_item.json")
+argc = len(sys.argv)
+for idx in range(1, argc):
+    loadFile.append(sys.argv[idx])
+save_to_json_file(loadFile, "add_item.json"
