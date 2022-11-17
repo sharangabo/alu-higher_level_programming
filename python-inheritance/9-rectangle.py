c8261962243cb159347af37f468228448c7d32be
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 <e.sharangab@alustudent.com>
2022-11-17 01:10:15 -0800

    commit

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,7 +2,7 @@
 '''Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)'''
 
 
-BaseGeometry = __import__('7-base_geometry').
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
