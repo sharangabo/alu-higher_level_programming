9f59ca3774da7b5b28595afe5c4e214cc73f8394
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 <e.sharangab@alustudent.com>
2022-11-17 03:45:23 -0800

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file:"""
+"""adds all arguments to a Python list, and then save them to a file:
+"""
 
 
 import sys
