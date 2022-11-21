4f95cde03b773f069371b50de4f27fff6abecf18
sharangaba <e.sharangab@alustudent.com>
2022-11-21 15:54:12 +0000

    commit

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
                 print("[{}] {}".format(
                     json_response["id"],
                     json_response["name"])
-            )
+                )
             else:
                 print("No result")
     except:
