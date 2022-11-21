0360eb31f6b8ec4f07c502b0d24a036993db78ea
sharangaba <e.sharangab@alustudent.com>
2022-11-21 23:54:12 +0000

    commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
-cript that creates the table 'force_name' on your MySQL server
+-- script that creates the table 'force_name' on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
-cript that creates the table 'unique_id' on your MySQL server
+-- script that creates the table 'unique_id' on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
