f1b4634c222b125b8ed58ec3473c1149f59786a8
sharangaba <e.sharangab@alustudent.com>
2022-11-22 00:09:08 +0000

    commit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,2 +1,8 @@
--- script that creates the table 'unique_id' on your MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- script that creates the database 'hbtn_0d_usa' and the table 'states',
+-- (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
