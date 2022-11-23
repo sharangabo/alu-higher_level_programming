b41f717a33cf0cbf6c4b2329e78cb5177ad6aa92
sharangaba <e.sharangab@alustudent.com>
2022-11-23 07:57:39 +0000

    commit

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""call content
-"""
-
+"""A python script that takes in a url,
+sends a request to url and displays the body of the
+response decoded in utf-8"""
 import urllib.request
-import urllib.error
 import sys
 
-if __name__ == '__main__':
-    """"Message"""
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
+
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(req) as response:
-            data = response.read()
-            print("{}".format(data.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(e.reason)
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
