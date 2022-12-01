e20e56241d411dc71cd01ae23f19d37f98f7bb14
sharangaba <e.sharangab@alustudent.com>
2022-12-01 18:08:26 +0000

    commit

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@ def text_indentation(text):
     '''Fucntion to bring the texts'''
     if type(text) != str:
         raise TypeError("text must be a string")
-    
+ 
     for delim in ".:?":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)]
