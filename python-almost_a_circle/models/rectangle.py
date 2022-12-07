551c7d4d0c5dc8c1b703ba2214376a0b7e250043
sharangabo <e.sharangab@alustudent.com>
2022-12-07 11:32:54 +0200

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,6 +28,7 @@ class Rectangle(Base):
 
         if type(value) != int:
             raise TypeError("width must be an integer")
+
         if value <= 0:
             raise ValueError("width must be > 0")
 
